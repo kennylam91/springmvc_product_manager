fd158302b900dd756e4adbad5ffaef5c087198d6
kennylam91 <phamvanlam1991@gmail.com>
2019-07-02 14:05:56 +0700

    add com.codegym.controllers add views test springmvc-controllers-views: hello world: OK

---
+++ src/main/java/com/codegym/controllers/ProductManager.java
@@ -0,0 +1,14 @@
+package com.codegym.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductManager {
+    @GetMapping
+    public String test(){
+        return "test";
+    }
+}
