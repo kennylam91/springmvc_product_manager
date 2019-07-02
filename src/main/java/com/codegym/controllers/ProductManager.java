de37b1cde0c248442bd5ddb17842969c35edad58
kennylam91 <phamvanlam1991@gmail.com>
2019-07-02 15:29:39 +0700

    add Product model

--- src/main/java/com/codegym/controllers/ProductManager.java
+++ src/main/java/com/codegym/controllers/ProductManager.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductManager {
     @GetMapping
     public String test(){
-        return "test";
+        return "list";
     }
 }
